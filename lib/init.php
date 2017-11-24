d6055af0933b8d8a878592222a9d929a3f93d84d
jhenifer <jhenifer@dgtalliance.com>
2017-11-24 17:29:04 -0500

    cambiando credenciales

--- lib/init.php
+++ lib/init.php
@@ -1,15 +1,9 @@
 <?php
+require_once 'credentials.php';
 require_once 'db.class.php';
 require_once 'Paginator.php';
 global $db;
 
 $db = new DbMysql();
-
-$credentials = array(
-    'hostname' => 'localhost',
-    'database' => 'chicago',
-    'username' => 'root',
-    'password' =>  ''
-);
 $db->connect($credentials);
 
